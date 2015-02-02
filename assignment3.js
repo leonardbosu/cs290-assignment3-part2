43ce9d640a6429395c9f10740979603bc02c6473
leonardbosu <leonardb@onid.oregonstate.edu>
2015-02-01 17:20:39 -0800

    added AJAX request and page modifier

--- assignment3.js
+++ assignment3.js
@@ -23,3 +23,45 @@ function saveSettings()
 	localStorage.setItem("userStoreLangSQL",userSetLangSQL.checked);
 
 }
+
+function sendRequest()
+{
+	var req = new XMLHttpRequest();
+	if (!req)
+	{
+		throw 'Unable to create HTTPRequest';
+	}
+
+	var url = "https://api.github.com/gists/public";
+
+	req.onreadystatechange = function()
+	{
+		//process sever request
+		if (this.readyState === 4)
+		{
+			var resp = JSON.parse(this.responseText);
+			localStorage.setItem("ServerResponse", resp);
+		}
+
+	}
+
+	url = createURL(url);
+
+	req.open('GET', url, true);
+	req.send(null);
+
+}
+
+function createURL(initialURL)
+{
+	//?page=3
+	var pageCheck = localStorage.getItem("userStorePages");
+	var newURL = initialURL;
+
+	if (pageCheck > 0)
+	{
+		newURL += ("?page=" + pageCheck);
+	}
+	
+	return newURL;
+}
